7a9557c664718b9f85c7bc68725230de82fa3b7e
benmelz <ben@melz.me>
2023-10-18 13:34:19 -0400

    add sections layer

---
+++ src/layers/sections.js
@@ -0,0 +1,10 @@
+import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
+
+export default new FeatureLayer({
+  portalItem: {
+    id: '7f8f103d89b544b9b13b1fd7ffcd4338',
+  },
+  layerId: 1,
+  definitionExpression: `SectionPublicVisible = 'Yes'`,
+  outFields: ['*'],
+});
--- src/map.js
+++ src/map.js
@@ -1,10 +1,11 @@
 import Map from '@arcgis/core/Map';
 import buildings from './layers/buildings.js';
+import sections from './layers/sections.js';
 
 /**
  * Manages the data sources for the application.
  */
 export default new Map({
   basemap: 'topo-vector',
-  layers: [buildings],
+  layers: [sections, buildings],
 });
