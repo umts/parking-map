495eb86901266fab7e81c2743d9cddb8457501d7
benmelz <ben@melz.me>
2023-10-18 13:56:59 -0400

    add basic renderer

--- src/layers/sections.js
+++ src/layers/sections.js
@@ -6,5 +6,28 @@ export default new FeatureLayer({
   },
   layerId: 1,
   definitionExpression: `SectionPublicVisible = 'Yes'`,
-  outFields: ['*'],
+  renderer: {
+    type: 'unique-value',
+    field: 'SectionColor',
+    defaultSymbol: {type: 'simple-fill'},
+    uniqueValueInfos: [{
+      value: 'Blue',
+      symbol: {type: 'simple-fill', color: [0, 0, 192, 0.5], outline: {color: '#0000bf'}},
+    }, {
+      value: 'Green',
+      symbol: {type: 'simple-fill', color: [0, 128, 0, 0.5], outline: {color: '#008000'}},
+    }, {
+      value: 'Pink',
+      symbol: {type: 'simple-fill', color: [255, 0, 255, 0.5], outline: {color: '#ff00ff'}},
+    }, {
+      value: 'Purple',
+      symbol: {type: 'simple-fill', color: [128, 0, 128, 0.5], outline: {color: '#800080'}},
+    }, {
+      value: 'Red',
+      symbol: {type: 'simple-fill', color: [192, 0, 0, 0.5], outline: {color: '#bf0000'}},
+    }, {
+      value: 'Yellow',
+      symbol: {type: 'simple-fill', color: [192, 192, 0, 0.5], outline: {color: '#bfbf00'}},
+    }],
+  },
 });
