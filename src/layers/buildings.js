1ff99a38dd518dcc5ab6a8c61a6e07bbdb9425a1
benmelz <ben@melz.me>
2023-10-18 12:55:13 -0400

    add buildings layer

---
+++ src/layers/buildings.js
@@ -0,0 +1,17 @@
+import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
+
+export default new FeatureLayer({
+  portalItem: {
+    id: 'c048f4a9df6344b19281876740c44ba0',
+  },
+  renderer: {
+    type: 'simple',
+    symbol: {
+      type: 'simple-fill',
+      color: [0, 0, 0, 0],
+      outline: {
+        width: 0,
+      },
+    },
+  },
+});
