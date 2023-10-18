06bd50debbe985afb3d06bb648d4f08b5a74cb25
benmelz <ben@melz.me>
2023-10-18 13:21:42 -0400

    add documentation

--- src/layers/buildings.js
+++ src/layers/buildings.js
@@ -1,5 +1,13 @@
 import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
 
+/**
+ * Campus buildings data layer.
+ *
+ *   - rendering is disabled due to buildings being a part of both base maps
+ *   - labels are a part of the vector base map, so they are only shown on the satellite base map
+ *
+ * @see {basemapToggle} for label show/hide logic.
+ */
 export default new FeatureLayer({
   portalItem: {
     id: 'c048f4a9df6344b19281876740c44ba0',
--- src/widgets/basemapToggle.js
+++ src/widgets/basemapToggle.js
@@ -10,6 +10,7 @@ const basemapToggle = new BasemapToggle({
   nextBasemap: 'satellite',
 });
 
+// Labels (building names) are already part of the vector basemap, so only show them for the satellite basemap.
 basemapToggle.watch('activeBasemap', (basemap) => {
   buildings.labelsVisible = basemap.id === 'satellite';
 });
