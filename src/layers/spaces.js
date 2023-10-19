56a0792199d3f5464ecc73ebba7a5182a2e312ea
benmelz <ben@melz.me>
2023-10-19 12:06:13 -0400

    add spaces layer

---
+++ src/layers/spaces.js
@@ -0,0 +1,8 @@
+import FeatureLayer from '@arcgis/core/layers/FeatureLayer';
+
+export default new FeatureLayer({
+  portalItem: {
+    id: '7f8f103d89b544b9b13b1fd7ffcd4338',
+  },
+  layerId: 0,
+});
