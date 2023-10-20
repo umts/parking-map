11f6539ed0be326a2354d3663a5aaa9db1c91283
benmelz <ben@melz.me>
2023-10-20 11:38:32 -0400

    add search widget

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import view from './view.js';
 import basemapToggle from './widgets/basemapToggle.js';
 import home from './widgets/home.js';
 import locate from './widgets/locate.js';
+import search from './widgets/search.js';
 import zoom from './widgets/zoom.js';
 
+view.ui.add(search, 'top-left');
 view.ui.add(zoom, 'top-left');
 view.ui.add(home, 'top-left');
 view.ui.add(locate, 'top-left');
