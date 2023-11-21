954a5ab740ec19b8d701c5b0435f8d1c66967169
benmelz <ben@melz.me>
2023-11-21 10:59:01 -0500

    add docs

--- src/widgets/filter.js
+++ src/widgets/filter.js
@@ -2,6 +2,9 @@ import Expand from '@arcgis/core/widgets/Expand';
 import LayerList from '@arcgis/core/widgets/LayerList';
 import view from '../view.js';
 
+/**
+ * Widget that controls feature visibility.
+ */
 export default new Expand({
   view: view,
   expandIcon: 'layers',
